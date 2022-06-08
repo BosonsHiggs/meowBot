7a0d15cb5b6d14224fbedcb0781ecb9fc66e5e05
iagolirapassos <iagolirapassos@gmail.com>
2022-06-08 17:12:02 -0300

    Second commit

--- classes/applications.py
+++ classes/applications.py
@@ -14,26 +14,26 @@ from discord.ui import Modal, TextInput
 # that user has to answer. The callback function
 # of this class is called when the user submits the modal
 class MyModal(Modal):
-	def __init__(self, title:str=None, custom_id:str=None, timeout:int=None, *args) -> None:
-		super().__init__(timeout=timeout, title=title, custom_id=custom_id)
+  def __init__(self, title:str=None, custom_id:str=None, timeout:int=None, *args) -> None:
+    super().__init__(timeout=timeout, title=title, custom_id=custom_id)
 
-		for form in args[0]:
-			self.add_item(
-							TextInput(
-								label = form[0],
-								placeholder = form[1],
-								style = form[2],
-								max_length = form[3],
-								required = form[4]
-							)
-						)
+    for form in args[0]:
+      self.add_item(
+              TextInput(
+                label = form[0],
+                placeholder = form[1],
+                style = form[2],
+                max_length = form[3],
+                required = form[4]
+              )
+            )
 
 
-	async def on_submit(self, interaction: discord.Interaction):
-		await interaction.response.send_message(f"Submitting form! Wait!... {self.children[0].value}, {self.children[1].value}")
+  async def on_submit(self, interaction: discord.Interaction):
+    await interaction.response.send_message(f"Submitting form! Wait!... {self.children[0].value}, {self.children[1].value}")
 
-	async def on_error(self, interaction: discord.Interaction, error: Exception) -> None:
-		await interaction.response.send_message('Oops! Something went wrong.', ephemeral=True)
+  async def on_error(self, interaction: discord.Interaction, error: Exception) -> None:
+    await interaction.response.send_message('Oops! Something went wrong.', ephemeral=True)
 
 ## Botões e listas
 ########################################
@@ -43,90 +43,90 @@ class MyModal(Modal):
 ########################################
 
 class Button(discord.ui.Button["GeneralButton"]):
-	def __init__(self, emoji, label, urls, style, return_, x: int, y: int, author, cont_custom, enable:bool=None, stop_:bool=True, modal:Modal=None):
-		super().__init__(style=style, emoji=emoji, label=label, url=urls, row=y, custom_id=cont_custom)
-		
-		self.emoji = emoji
-		self.label = label
-		self.style = style
-		self.return_ = return_
-		self.author = author
-		self.url = urls
-		self.enable = enable
-		self.custom_id = cont_custom
-		self.stop_ = stop_
-		self.modal = modal
-		
-	async def callback(self, interaction: discord.Interaction):
-		assert self.view is not None
-		view: GeneralButton = self.view
+  def __init__(self, emoji, label, urls, style, return_, x: int, y: int, author, cont_custom, enable:bool=None, stop_:bool=True, modal:Modal=None):
+    super().__init__(style=style, emoji=emoji, label=label, url=urls, row=y, custom_id=cont_custom)
+    
+    self.emoji = emoji
+    self.label = label
+    self.style = style
+    self.return_ = return_
+    self.author = author
+    self.url = urls
+    self.enable = enable
+    self.custom_id = cont_custom
+    self.stop_ = stop_
+    self.modal = modal
+    
+  async def callback(self, interaction: discord.Interaction):
+    assert self.view is not None
+    view: GeneralButton = self.view
 
-		self.disabled = self.enable
-		self.label = self.label
-		self.style = self.style
-		self.url = self.url
-		view.disable = self.enable
-		
-		#await interaction.response.send_message('waiting...', delete_after=1)
-		#await interaction.response.defer(ephemeral=True)
+    self.disabled = self.enable
+    self.label = self.label
+    self.style = self.style
+    self.url = self.url
+    view.disable = self.enable
+    
+    #await interaction.response.send_message('waiting...', delete_after=1)
+    #await interaction.response.defer(ephemeral=True)
 
-		"""if str(self.emoji) == "🎟️" and self.modal is None:
-			await interaction.response.defer(ephemeral=True)
-			guild_id = interaction.guild_id
-			channel = interaction.channel
-			guild = interaction.guild
-			author = interaction.user
+    """if str(self.emoji) == "🎟️" and self.modal is None:
+      await interaction.response.defer(ephemeral=True)
+      guild_id = interaction.guild_id
+      channel = interaction.channel
+      guild = interaction.guild
+      author = interaction.user
 
-			await newticket(guild, channel, author, interaction)"""
-		if self.modal is not None:
-			await interaction.response.send_modal(self.modal)
-			return
-		else:
-			await interaction.response.defer()
-			
-		view.value = self.return_
-		view.interaction = interaction
-		if self.stop_: view.stop()
+      await newticket(guild, channel, author, interaction)"""
+    if self.modal is not None:
+      await interaction.response.send_modal(self.modal)
+      return
+    else:
+      await interaction.response.defer()
+      
+    view.value = self.return_
+    view.interaction = interaction
+    if self.stop_: view.stop()
 
 class GeneralButton(discord.ui.View):
-	def __init__(self, 
-				emoji:str=None, 
-				label:str=None, 
-				urls:str=None, 
-				style:discord.ButtonStyle=None, 
-				author:discord.Member=None, 
-				enable:bool=True, 
-				stop:bool=True, 
-				messageid:int=None,
-				modal:Modal = None
-	):
-		super().__init__(timeout=None)
-		self.label = label
-		self.style = style
-		self.emoji = emoji
-		self.urls = urls
-		self.enable = enable
-		self.stop_ = stop
-		self.messageid	= messageid
-		self.modal = modal
-		
-		#self.value = None
-		
-		cont_x = 0
-		cont_y = 0
-		cont_custom = 0
-		for a in range(len(style)):
-			if urls[a] is not None:
-				self.add_item(Button(self.emoji[a], self.label[a], self.urls[a], self.style[a], a, cont_x, cont_y, None, None, self.enable, self.stop_, self.modal))
-			else:
-				if self.messageid is None:
-					self.add_item(Button(self.emoji[a], self.label[a], self.urls[a], self.style[a], a, cont_x, cont_y, author, str(cont_custom), self.enable, self.stop_, self.modal))
-				else:
-					self.add_item(Button(self.emoji[a], self.label[a], self.urls[a], self.style[a], a, cont_x, cont_y, author, str(self.messageid), self.enable, self.stop_, self.modal))
+  def __init__(self, 
+        emoji:str=None, 
+        label:str=None, 
+        urls:str=None, 
+        style:discord.ButtonStyle=None, 
+        author:discord.Member=None, 
+        enable:bool=True, 
+        stop:bool=True, 
+        messageid:int=None,
+        modal:Modal = None
+  ):
+    super().__init__(timeout=None)
+    self.label = label
+    self.style = style
+    self.emoji = emoji
+    self.urls = urls
+    self.enable = enable
+    self.stop_ = stop
+    self.messageid	= messageid
+    self.modal = modal
+    
+    #self.value = None
+    
+    cont_x = 0
+    cont_y = 0
+    cont_custom = 0
+    for a in range(len(style)):
+      if urls[a] is not None:
+        self.add_item(Button(self.emoji[a], self.label[a], self.urls[a], self.style[a], a, cont_x, cont_y, None, None, self.enable, self.stop_, self.modal))
+      else:
+        if self.messageid is None:
+          self.add_item(Button(self.emoji[a], self.label[a], self.urls[a], self.style[a], a, cont_x, cont_y, author, str(cont_custom), self.enable, self.stop_, self.modal))
+        else:
+          self.add_item(Button(self.emoji[a], self.label[a], self.urls[a], self.style[a], a, cont_x, cont_y, author, str(self.messageid), self.enable, self.stop_, self.modal))
 
-			cont_custom+=1
-			cont_x+=1
+      cont_custom+=1
+      cont_x+=1
 
-			if cont_x == 5: 
-				cont_x = 0
-				cont_y +=1
+      if cont_x == 5: 
+        cont_x = 0
+        cont_y +=1
--- classes/users.py
+++ classes/users.py
@@ -5,50 +5,66 @@ from discord import app_commands
 from classes.applications import MyModal, GeneralButton
 
 class Users(commands.Cog):
-	def __init__(self, bot: commands.Bot) -> None:
-		self.bot: commands.Bot = bot
+  def __init__(self, bot: commands.Bot) -> None:
+    self.bot: commands.Bot = bot
 
-	##Groups
-	@commands.has_permissions(administrator = False)
-	@commands.hybrid_group(name="add")
-	async def add_command(self, ctx: commands.Context):
-		if ctx.invoked_subcommand is None:
-			await ctx.send(f'Error! Error in add_command!', ephemeral=True)
+  ##Groups
+  @commands.has_permissions(administrator = False)
+  @commands.hybrid_group(name="add")
+  async def add_command(self, ctx: commands.Context):
+    if ctx.invoked_subcommand is None:
+      await ctx.send(f'Error! Error in add_command!', ephemeral=True)
 
-	##Commands
-	@commands.cooldown(1, 10, commands.BucketType.guild)
-	@add_command.command(name="newform", brief="Moderation・create a new form!")
-	async def newform_command(
-			self, 
-			ctx: commands.Context,
-			):
-		await ctx.defer()
-		user1 = ctx.author
-		messageid = ctx.message.id
-		field = []
+  ##Commands
+  @commands.cooldown(1, 10, commands.BucketType.guild)
+  @add_command.command(name="newform", brief="Moderation・create a new form!")
+  async def newform_command(
+      self, 
+      ctx: commands.Context,
+      ):
+    await ctx.defer()
+    user1 = ctx.author
+    messageid = ctx.message.id
+    field = []
 
-		##Button
-		emojis = ('✅')
-		styles = tuple(discord.ButtonStyle.green for emoji in emojis)
-		labels = tuple(None for emoji in emojis)
-		urls = tuple(None for emoji in emojis)
-		
-		##Modal: max 5 questions
-		field.append(("Name", 'Your name here...', discord.TextStyle.long, 300, True))
-		field.append(("Phone", 'Your phone number here...', discord.TextStyle.short, 10, True))
-		field.append(("Email1", 'Your Email here...', discord.TextStyle.short, 30, True))
-		field.append(("Email2", 'Your Email here...', discord.TextStyle.short, 30, True))
-		field.append(("Email3", 'Your Email here...', discord.TextStyle.short, 30, True))
+    ##Button
+    emojis = ('✅')
+    styles = tuple(discord.ButtonStyle.green for emoji in emojis)
+    labels = tuple(None for emoji in emojis)
+    urls = tuple(None for emoji in emojis)
+    
+    ##Modal: max 5 questions
+    field.append(("Name", 'Your name here...', discord.TextStyle.long, 300, True))
+    field.append(("Phone", 'Your phone number here...', discord.TextStyle.short, 10, True))
+    field.append(("Email1", 'Your Email here...', discord.TextStyle.short, 30, True))
+    field.append(("Email2", 'Your Email here...', discord.TextStyle.short, 30, True))
+    field.append(("Email3", 'Your Email here...', discord.TextStyle.short, 30, True))
 
-		myModal = MyModal("Job", "123456789", None, field)
-		view = GeneralButton(emojis, labels, urls, styles, user1, False, True, messageid, myModal)
-		
-		#await ctx.interaction.response.send_modal(myModal)
-		await ctx.send(content="Press!", view=view)
-		
+    myModal = MyModal("Job", "123456789", None, field)
+    view = GeneralButton(emojis, labels, urls, styles, user1, False, True, messageid, myModal)
+    
+    #await ctx.interaction.response.send_modal(myModal)
+    await ctx.send(content="Press!", view=view)
+
+  @commands.hybrid_command(name="fruits")
+  async def fruits_command(self, ctx:commands.Context, fruits: str):
+    await ctx.interaction.response.send_message(f'Your favourite fruit seems to be {fruits}')
+
+  @fruits_command.autocomplete('fruits')
+  async def fruits_autocomplete(
+    ctx: commands.Context,
+    namespace: app_commands.Namespace,
+    current: str
+  ) -> List[app_commands.Choice[str]]:
+    fruits = ['Banana', 'Pineapple', 'Apple', 'Watermelon', 'Melon', 'Cherry']
+    return [
+      app_commands.Choice(name=fruit, value=fruit)
+      for fruit in fruits if current.lower() in fruit.lower()
+    ]
+    
 async def setup(bot: commands.Bot) -> None:
-	TEST_GUILD = Test_Server_ID
-	#global sync
-	#await bot.add_cog(Admins(bot))
-	#Local sync
-	await bot.add_cog(Users(bot), guilds=bot.get_guild(TEST_GUILD))
+  TEST_GUILD = Test_Server_ID
+  #global sync
+  #await bot.add_cog(Admins(bot))
+  #Local sync
+  await bot.add_cog(Users(bot), guilds=bot.get_guild(TEST_GUILD))
